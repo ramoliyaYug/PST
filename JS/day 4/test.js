f9a55a273ac7f29903b3ffc6139f2fa1916e4d81
ramoliyaYug <mastermind57369@gmail.com>
2024-09-09 15:55:45 +0530

    questions

--- JS/day 4/test.js
+++ JS/day 4/test.js
@@ -1,5 +1,57 @@
-const fruits = ["apple", "orange", "watermaelon", "grapes", "cherry"];
+//questionn 1
 
-fruits.fill = ("kiwi",2,5) ;
+let emp1Name = "YUG"
+let emp1Age = 17
+let emp1Salary = "200000lpd"
 
-console.log(fruits);
+let emp2Name = "OM"
+let emp2Age = 17
+let emp2Salary = "300000lpw"
+
+let emp3Name = "KEERTHIKA"
+let emp3Age = 18
+let emp3Salary = "400000lph"
+
+// question 2
+//with object
+const employee = {
+    person1 : {
+        name : "YUG",
+        age : 17,
+        salary : "200000lpd"
+    },
+    person2 : {
+        name : "OM",
+        age : 17,
+        salary : "300000lpw"
+    },
+    person3 : {
+        name : "KEERTHIKA",
+        age : 18,
+        salary : "400000lph"
+    }
+}
+
+//with array
+const employee2 = [
+    {
+        name : "YUG",
+        age : 17,
+        salary : "200000lpd"
+    },
+
+    {
+        name : "OM",
+        age : 17,
+        salary : "300000lpw"
+    },
+    {
+        name : "KEERTHIKA",
+        age : 18,
+        salary : "400000lph"
+    }
+]
+
+//DIFF B/W [] AND . when calling property
+//what are the rule to define key in object
+//what are the specific reasons to use object or array
