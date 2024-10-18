2be2c58a3b19d692c62711b87aa1c03480b0ddd9
ramoliyaYug <mastermind57369@gmail.com>
2024-10-18 10:37:45 +0530

    java day 4 commit

---
+++ java/day 05/polymorphism/Polymorphism.java
@@ -0,0 +1,12 @@
+/*
+Polymorphism:
+1.compile time polymorphism:
+ - if number of parameter are same then atleast one parameter should have different datatype.
+ - if datatype is same then they should have different number of parameters.
+ Ex:-
+add(int a,int b)
+add(int a,int b,int c)
+*/
+public class Polymorphism {
+    
+}
