0eae9324130d1350da1d7ce3179b7960979dac65
ramoliyaYug <mastermind57369@gmail.com>
2024-10-20 22:40:23 +0530

    java commit

--- java/day 05/polymorphism/Polymorphism.java
+++ java/day 05/polymorphism/Polymorphism.java
@@ -7,6 +7,32 @@ Polymorphism:
 add(int a,int b)
 add(int a,int b,int c)
 */
+class Parent{
+    public void mul(int a,int b){
+        System.out.println("this is multiplication: "+(a*b));
+    }
+}
+
+class Child extends Parent{
+    public void mul(int a,int b){
+        System.out.println("multiplication: "+(a*b));
+    }
+}
 public class Polymorphism {
-    
+    //compiletime polymorphism
+    static void add(int a,int b){
+        System.out.println("this is sum: " + (a+b));
+    }
+    //overloading
+    static void add(float a,int b){
+        System.out.println("sum: " + (a+b));
+    }
+    public static void main(String[] args) {
+        add(1,2);
+        add(1,2);
+        Child ch = new Child();
+        Parent pt = new Parent();
+        pt.mul(1,2);
+        ch.mul(1,3);
+    }
 }
