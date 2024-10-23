6cfa6a6797b72cd63aee78d4b808e95b5b638f70
ramoliyaYug <mastermind57369@gmail.com>
2024-10-23 23:02:52 +0530

    dsa java

--- DSA/JAVA/day 02/Last.java
+++ DSA/JAVA/day 02/Last.java
@@ -3,5 +3,18 @@ public class Last {
         String word = "Hello World";
         String[] arr = word.split(" ");
         System.out.println(arr[arr.length - 1].length());
-    }    
+
+        // another approach
+        int counter = 0;
+        boolean isWordStarted =  false;
+        for(int i = word.length() - 1; i >= 0; i--) {
+            if(word.charAt(i) != ' ') {
+                counter++;
+                isWordStarted = true;
+            }else if(isWordStarted){
+                break;
+            }
+        }
+        System.out.println(counter);
+        }    
 }
