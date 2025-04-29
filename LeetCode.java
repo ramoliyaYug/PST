72795b1f18aa5d315bf0ff94a21eb0a9ead18e1c
ramoliyaYug <mastermind57369@gmail.com>
2025-04-29 09:57:54 +0530

    added the mern things

--- LeetCode.java
+++ LeetCode.java
@@ -1,31 +1,36 @@
 /*
-Given a positive integer n, find the pivot integer x such that:
+Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
 
-The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
-Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+The objective of the game is to end with the most stones. The total number of stones across all the piles is odd, so there are no ties.
+
+Alice and Bob take turns, with Alice starting first. Each turn, a player takes the entire pile of stones either from the beginning or from the end of the row. This continues until there are no more piles left, at which point the person with the most stones wins.
+
+Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins.
 
  
 
 Example 1:
 
-Input: n = 8
-Output: 6
-Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+Input: piles = [5,3,4,5]
+Output: true
+Explanation: 
+Alice starts first, and can only take the first 5 or the last 5.
+Say she takes the first 5, so that the row becomes [3, 4, 5].
+If Bob takes 3, then the board is [4, 5], and Alice takes 5 to win with 10 points.
+If Bob takes the last 5, then the board is [3, 4], and Alice takes 4 to win with 9 points.
+This demonstrated that taking the first 5 was a winning move for Alice, so we return true.
 Example 2:
 
-Input: n = 1
-Output: 1
-Explanation: 1 is the pivot integer since: 1 = 1.
-Example 3:
-
-Input: n = 4
-Output: -1
-Explanation: It can be proved that no such integer exist.
+Input: piles = [3,7,2,3]
+Output: true
  
 
 Constraints:
 
-1 <= n <= 1000
+2 <= piles.length <= 500
+piles.length is even.
+1 <= piles[i] <= 500
+sum(piles[i]) is odd.
 */
 
 import java.util.List;
@@ -40,29 +45,31 @@ import java.math.BigInteger;
 
 
 public class LeetCode {
-    public int pivotInteger(int n) {
-        int x = -1;
-        for(int i = 1; i <= n; i++) {
-            if(sum(1, i) == sum(i, n)) {
-                x = i;
-                break;
+    public boolean stoneGame(int[] piles) {
+        int alice = 0;
+        int bob = 0;
+        int n = piles.length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(piles[i]);
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                alice += list.get(i);
+            } else {
+                bob += list.get(i);
             }
-        }
-        return x;
-    }
-
-    public static int sum(int start, int end) {
-        int res = 0;
-        for(int i = start; i <= end; i++) {
-            res += i;
-        }
-        return res;
+        }       
+        return alice > bob;
     }
 
     public static void main(String[] args) {
         LeetCode lc = new LeetCode();
-        System.out.println(lc.pivotInteger(8)); // Output: 6
-        System.out.println(lc.pivotInteger(1)); // Output: 1
-        System.out.println(lc.pivotInteger(4)); // Output: -1
+        int[] piles1 = {5, 3, 4, 5};
+        int[] piles2 = {3, 7, 2, 3};
+        System.out.println(lc.stoneGame(piles1)); // Output: true
+        System.out.println(lc.stoneGame(piles2)); // Output: true
+        
     }
 }
