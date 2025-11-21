ba3e7e541a6781999371eb97abef5c02d536858d
ramoliyaYug <mastermind57369@gmail.com>
2025-11-21 13:56:10 +0530

    cleaned the life expectancy data set

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -1,46 +1,91 @@
 /*
-Emuskald needs a fence around his farm, but he is too lazy to build it himself. So he purchased a fence-building robot.
+Kuriyama Mirai has killed many monsters and got many (namely n) stones. She numbers the stones from 1 to n. The cost of the i-th stone is vi. Kuriyama Mirai wants to know something about these stones so she will ask you two kinds of questions:
 
-He wants the fence to be a regular polygon. The robot builds the fence along a single path, but it can only make fence corners at a single angle a.
-
-Will the robot be able to build the fence Emuskald wants? In other words, is there a regular polygon which angles are equal to a?
+She will tell you two numbers, l and r (1 ≤ l ≤ r ≤ n), and you should tell her .
+Let ui be the cost of the i-th cheapest stone (the cost that will be on the i-th place if we arrange all the stone costs in non-decreasing order). This time she will tell you two numbers, l and r (1 ≤ l ≤ r ≤ n), and you should tell her .
+For every question you should give the correct answer, or Kuriyama Mirai will say "fuyukai desu" and then become unhappy.
 
 Input
-The first line of input contains an integer t (0 < t < 180) — the number of tests. Each of the following t lines contains a single integer a (0 < a < 180) — the angle the robot can make corners at measured in degrees.
+The first line contains an integer n (1 ≤ n ≤ 105). The second line contains n integers: v1, v2, ..., vn (1 ≤ vi ≤ 109) — costs of the stones.
+
+The third line contains an integer m (1 ≤ m ≤ 105) — the number of Kuriyama Mirai's questions. Then follow m lines, each line contains three integers type, l and r (1 ≤ l ≤ r ≤ n; 1 ≤ type ≤ 2), describing a question. If type equal to 1, then you should output the answer for the first question, else you should output the answer for the second one.
 
 Output
-For each test, output on a single line "YES" (without quotes), if the robot can build a fence Emuskald wants, and "NO" (without quotes), if it is impossible.
+Print m lines. Each line must contain an integer — the answer to Kuriyama Mirai's question. Print the answers to the questions in the order of input.
 
 Examples
 InputCopy
+6
+6 4 2 7 2 7
 3
-30
-60
-90
+2 3 6
+1 3 4
+1 1 6
 OutputCopy
-NO
-YES
-YES
+24
+9
+28
+InputCopy
+4
+5 5 2 3
+10
+1 2 4
+2 1 4
+1 1 1
+2 1 4
+2 1 2
+1 1 1
+1 3 3
+1 1 3
+1 4 4
+1 2 2
+OutputCopy
+10
+15
+5
+15
+5
+5
+2
+12
+3
+5
 Note
-In the first test case, it is impossible to build the fence, since there is no regular polygon with angle .
-
-In the second test case, the fence is a regular triangle, and in the last test case — a square.
+Please note that the answers to the questions may overflow 32-bit integer type.
 */
 #include <bits/stdc++.h>
 using namespace std;
 void solve() {
-    int a;
-    cin >> a;
-    if (360 % (180 - a) == 0) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
+    int n;
+    cin >> n;
+    vector<long long> arr(n);
+    for(int i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+    vector<long long> sortedArr = arr;
+    sort(sortedArr.begin(), sortedArr.end());
+    vector<long long> prefixSum(n+1, 0);
+    vector<long long> sortedPrefixSum(n+1, 0);
+    for(int i = 1;i<=n;i++){
+        prefixSum[i] = prefixSum[i-1] + arr[i-1];
+        sortedPrefixSum[i] = sortedPrefixSum[i-1] + sortedArr[i-1];
+    }
+    int m;
+    cin >> m;
+    for(int i = 0;i<m;i++){
+        int type, l, r;
+        cin >> type >> l >> r;
+        if(type == 1){
+            cout << prefixSum[r] - prefixSum[l-1] << endl;
+        }else{
+            cout << sortedPrefixSum[r] - sortedPrefixSum[l-1] << endl;
+        }
     }
 }
 
 int main() {
     int t=1;
-    cin >> t;
+    // cin >> t;
     while (t--) {
         solve();
     }
--- LeetCode.cpp
+++ LeetCode.cpp
@@ -1,51 +1,43 @@
 /*
-You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
-
-To perform a flood fill:
-
-Begin with the starting pixel and change its color to color.
-Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
-Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
-The process stops when there are no more adjacent pixels of the original color to update.
-Return the modified image after performing the flood fill.
+Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
  
 
 Example 1:
 
-Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Input: n = 11
 
-Output: [[2,2,2],[2,2,0],[2,0,1]]
+Output: 3
 
 Explanation:
 
-
-
-From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
-
-Note the bottom corner is not colored 2, because it is not horizontally or vertically connected to the starting pixel.
+The input binary string 1011 has a total of three set bits.
 
 Example 2:
 
-Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Input: n = 128
 
-Output: [[0,0,0],[0,0,0]]
+Output: 1
 
 Explanation:
 
-The starting pixel is already colored with 0, which is the same as the target color. Therefore, no changes are made to the image.
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+
+Input: n = 2147483645
+
+Output: 30
+
+Explanation:
+
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
 
  
 
 Constraints:
 
-m == image.length
-n == image[i].length
-1 <= m, n <= 50
-0 <= image[i][j], color < 216
-0 <= sr < m
-0 <= sc < n
- 
+1 <= n <= 231 - 1
 
 */
 #include <bits/stdc++.h>
@@ -53,22 +45,8 @@ using namespace std;
 
 class LeetCode {
     public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int originalColor, int n, int m){
-        if(sr<0 || sc<0 || sr>=n || sc>=m) return;
-        if(image[sr][sc] != originalColor) return;
-        image[sr][sc] = color;
-        dfs(image, sr+1, sc, color, originalColor, n, m);
-        dfs(image, sr-1, sc, color, originalColor, n, m);
-        dfs(image, sr, sc+1, color, originalColor, n, m);
-        dfs(image, sr, sc-1, color, originalColor, n, m);
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        int originalColor = image[sr][sc];
-        if (originalColor == color) return image;
-        dfs(image, sr, sc, color, originalColor, n, m);
-        return image;
+    int hammingWeight(int n) {
+        return __builtin_popcount(n);
     }
 };
 
